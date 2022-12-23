3c4b0b6598ae354f7bbc528f791bccc75623fee3
Dmitry <markelovdima408@gmail.com>
2022-12-24 00:23:03 +0400

    commit 1

--- lab5/Program.cs
+++ lab5/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculator c = new Calculator();
+            //c.PressKey("1");
+            //c.PressKey("0");
+            //c.PressKey("+");
+            //c.PressKey("5");
+            //c.PressKey("=");
+            //Console.WriteLine(c.Screen);
+
+            double x = double.Parse("123.45", CultureInfo.InvariantCulture);
+            Console.WriteLine(x);
+        }
+    }
+}
